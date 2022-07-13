38f779923f2ce4750dc469445636d8a8c8db7e4b
王海明 <whaim@icloud.com>
2022-07-13 09:20:37 +0800

    add compiler and int type

--- predef.h
+++ predef.h
@@ -1,48 +1,79 @@
-//---------------------------------------------------------------
-//pre define os and archive 
-// OS: operating system
-//      OS_WINDOWS              Windows system
-//          OS_WIN_64           64bit Windows
-//          OS_WIN_32           32bit Windows
-//      OS_MACOS                MacOS system
-//      OS_IOS                  iPhone iOS system
-//      OS_ANDROID              android system
-//      OS_LINUX                linux system
-//      OS_UNKNOWN_APPLE        undefined apple system
-//      OS_UNKNOWN              undefined system
-//--------------------------------------------------------------
+
 #pragma once
+
+#include "cstdint"
+
+typedef int64_t     int64;
+typedef int32_t     int32;
+typedef int16_t     int16;
+typedef int8_t      int8;
+typedef uint64_t    uint64;
+typedef uint32_t    uint32;
+typedef uint16_t    uint16;
+typedef uint8_t     uint8;
 
 //--------------------------------------------------------------
 //Operating System
 //--------------------------------------------------------------
+#define OS_WINDOWS      1           //Windows system
+#define OS_WIN_32       11          //32bit Windows
+#define OS_WIN_64       12          //64bit Windows
+#define OS_LINUX        2           //linux system
+#define OS_MACOS        3           //MacOS system
+#define OS_IOS          31          //iPhone iOS system
+#define OS_UNDEF_APPLE  39          //undefined apple system
+#define OS_UNIX         4           //unix system
+#define OS_ANDROID      5           //android system
+#define OS_UNKNOWN      9           //undefined system
+
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
-    #define OS_WINDOWS
+#   define SYSTEM_OS OS_WINDOWS
 #ifdef _WIN64
-    #define OS_WIN_64
+#   define SYSTEM_OS OS_WIN_64
 #else
-    #define OS_WIN_32
+#   define SYSTEM_OS OS_WIN_32
 #endif
 
 #elif __APPLE__
 #include <TargetConditionals.h>
 #if defined(TARGET_OS_OSX)
-    #define OS_MACOS
+#define SYSTEM_OS OS_MACOS
 #elif TARGET_OS_IPHONE
-    #define OS_ICOS
+#define SYSTEM_OS OS_IOS
 #else
-    #define OS_UNDEF_APPLE
+#define SYSTEM_OS OS_UNDEF_APPLE
 #endif
 
 #elif __ANDROID__
-    #define OS_ANDROID
+#define SYSTEM_OS OS_ANDROID
 
 #elif __linux__
-    #define OS_LINUX
+#define SYSTEM_OS OS_LINUX
 
 #elif __unix__
-    #define OS_UNIX
+#define SYSTEM_OS OS_UNIX
 
 #else
-    #define OS_UNKNOWN
+#define SYSTEM_OS OS_UNKNOWN
+#endif
+
+//--------------------------------------------------------------
+//Compiler
+//--------------------------------------------------------------
+#define SYSTEM_COMPILER_MICROSOFT      1
+#define SYSTEM_COMPILER_BORLAND        2
+#define SYSTEM_COMPILER_INTEL          3
+#define SYSTEM_COMPILER_GNU            4
+
+#ifdef _MSC_VER
+#  define SYSTEM_COMPILER SYSTEM_COMPILER_MICROSOFT
+#elif defined( __BORLANDC__ )
+#  define SYSTEM_COMPILER SYSTEM_COMPILER_BORLAND
+#elif defined( __INTEL_COMPILER )
+#  define SYSTEM_COMPILER SYSTEM_COMPILER_INTEL
+#elif defined( __GNUC__ )
+#  define SYSTEM_COMPILER SYSTEM_COMPILER_GNU
+#  define GCC_VERSION (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__)
+#else
+#  error "FATAL ERROR: Unknown compiler."
 #endif
